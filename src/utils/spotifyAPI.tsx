595b80fbc7f3e0eca914c4784dce714b3e47c90b
Kelsey Morse-Brown <132404623+kelseymorsebrown@users.noreply.github.com>
2024-10-16 14:12:36 -0500

    Update redirect

--- src/utils/spotifyAPI.tsx
+++ src/utils/spotifyAPI.tsx
@@ -34,7 +34,10 @@ const spotifyAPI = {
     url += '&state=' + encodeURIComponent(state);
     url += '&show_dialog=true';
 
-    window.location.assign(url);
+    console.log(REDIRECT_URI);
+    console.log(process.env);
+
+    //window.location.assign(url);
   },
   getUser(stateKey: string, accessToken: string | null) {
     return fetch('https://api.spotify.com/v1/me', {
