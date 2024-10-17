b6d8aaa8be4546b50786773c5141a6e9daf2c70b
Kelsey Morse-Brown <132404623+kelseymorsebrown@users.noreply.github.com>
2024-10-17 11:13:17 -0500

    Fix callback for github pages

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,5 +1,5 @@
 export const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
 export const REDIRECT_URI =
   process.env.NODE_ENV === 'production'
-    ? 'https://kelseymorsebrown.github.io/jammming/callback'
-    : 'http://localhost:3000/callback';
+    ? 'https://kelseymorsebrown.github.io/jammming'
+    : 'http://localhost:3000';
