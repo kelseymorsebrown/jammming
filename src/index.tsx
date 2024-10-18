ee93ebd298cbf308c8922bb1b588161372721595
Kelsey Morse-Brown <132404623+kelseymorsebrown@users.noreply.github.com>
2024-10-18 13:38:56 -0500

    Remove deprecated error page reference

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './containers/App/App';
-import ErrorPage from './error-page';
 import reportWebVitals from './reportWebVitals';
 
 const rootElement = document.getElementById('root');
